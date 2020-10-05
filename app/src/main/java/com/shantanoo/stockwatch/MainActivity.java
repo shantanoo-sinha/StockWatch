6ca39b2c2154a108e3ddee73bf8992540435ce2d
Shantanoo Sinha <shantanoo.sinha@gmail.com>
2020-10-05 01:09:08 -0500

    Swipe refresh done

--- app/src/main/java/com/shantanoo/stockwatch/MainActivity.java
+++ app/src/main/java/com/shantanoo/stockwatch/MainActivity.java
@@ -1,9 +1,21 @@
 package com.shantanoo.stockwatch;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.shantanoo.stockwatch.adapter.StocksAdapter;
+import com.shantanoo.stockwatch.model.Stock;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
 
@@ -13,19 +25,64 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final String BASE_URL = "https://cloud.iexapis.com/stable/stock/";
     private static final String QUOTE_TOKEN = "quote?token=";
 
+    private List<Stock> stocks;
+
+    private RecyclerView recyclerView;
+    private SwipeRefreshLayout swipeRefreshLayout;
+
+    private StocksAdapter stocksAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.rvStocks);
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
+
+        stocks = new ArrayList<>();
+        setData();
+
+        stocksAdapter = new StocksAdapter(stocks, this);
+        recyclerView.setAdapter(stocksAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                //get data
+                Collections.shuffle(stocks);
+                stocksAdapter.notifyDataSetChanged();
+                swipeRefreshLayout.setRefreshing(false);
+                Toast.makeText(getApplicationContext(), "Data Refreshed", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    private void setData() {
+        stocks.add(new Stock("TSLA", "Tesla", 123.01, 23.01, 23.0));
+        stocks.add(new Stock("AMZN", "Amazon", 123.01, -23.01, 23.0));
+        stocks.add(new Stock("FCBK", "Facebook", 123.01, 23.01, 23.0));
+        stocks.add(new Stock("APPL", "Apple", 123.01, -23.01, 23.0));
+        stocks.add(new Stock("GOGL", "Google", 123.01, 23.01, 23.0));
+        stocks.add(new Stock("MSFT", "Microsoft", 123.01, -23.01, 23.0));
+    }
+
 
     @Override
     public void onClick(View v) {
-
+        Log.d(TAG, "onClick: ");
+        int position = recyclerView.getChildLayoutPosition(v);
+        Stock stock = stocks.get(position);
+        Toast.makeText(this, "On Click", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public boolean onLongClick(View v) {
+        Log.d(TAG, "onLongClick: ");
+        int position = recyclerView.getChildLayoutPosition(v);
+        Stock stock = stocks.get(position);
+        Toast.makeText(this, "On Long Click", Toast.LENGTH_SHORT).show();
         return false;
     }
 }
